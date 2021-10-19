b73ab74a6a9f84a5f1f0cff34505dac38f5bb347
João <hlabeats@gmail.com>
2021-10-18 22:44:02 -0300

    prepara variaveis de ambiente

--- conn/conn.js
+++ conn/conn.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
-const Conn = () => {
-    mongoose.connect('mongodb://localhost:27017/priori',
-    {useNewUrlParser: true,
-    useUnifiedTopology: true,}
+const Conn = (url, user, pass, data) => {
+    mongoose.connect(`${url}/${data}`,
+        {
+            user: user, 
+            pass: pass,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
     )
-.then(() => console.log('MongoDB conectado'))
-.catch((err)=> console.log('Erro na conexão com o banco de dados', err))
+        .then(() => console.log('MongoDB conectado'))
+        .catch((err) => console.log('Erro na conexão com o banco de dados', err))
 }
 
 module.exports = Conn;
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
 
@@ -8,9 +13,14 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-Conn();
+const db_url = process.env.DB_URL;
+const db_user = process.env.DB_USER;
+const db_pass = process.env.DB_PASS;
+const db_data = process.env.DB_DATA;
+
+Conn(db_url, db_user, db_pass, db_data);
 
 app.use('/priori', AppRoutes);
 
 const port = 3005;
-app.listen(port, () => console.log(`App rodando em  htttp://localhost:${port}/`))
+app.listen(process.env.PORT || port, () => console.log(`App rodando em  htttp://localhost:${port}/`))
